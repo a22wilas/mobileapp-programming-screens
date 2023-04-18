36c4973374ab07fc18751f719ab37a7c7bb9d27e
William Aspensten <a22wilas@student.his.se>
2023-04-18 16:18:01 +0200

    La till en knapp som genom find id kopplades till Button variabeln btn. Satte en click listener på variabeln vilket startar SecondActivity ifall knappen trycks på.

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,6 +3,8 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
+
     }
 }
